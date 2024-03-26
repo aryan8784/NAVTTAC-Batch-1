d9154d08d498ff5fc6f2184b8a506612a9e7837e
Muhammad Aryan Javed <aryanmali000@gmail.com>
2024-03-26 09:32:43 +0500

    DOM & FUNCTIONS

---
+++ DOM & Functions/script.js
@@ -0,0 +1,41 @@
+// Calculator
+// function getValue(e){
+//    let inp = document.getElementById("inp").value += e
+// }
+
+// function eq(){
+//     let inp = document.getElementById("inp")
+//     inp.value = eval(inp.value)
+// }
+
+
+// function clearAlll(){
+//     let inp = document.getElementById("inp");
+//     inp.value = ""
+// }
+
+// Calculator
+
+// =============================================
+
+// =========Random Color Generator
+// let colorCode = document.getElementById("color-Code");
+// let btn = document.getElementById("btn");
+
+
+// function getColor(){
+//     let randomNumber = Math.floor(Math.random() * 16777215)
+//     let randomCode = "#" + randomNumber.toString(16)
+//     document.body.style.backgroundColor = randomCode
+//     colorCode.innerHTML = randomCode;
+//     navigator.clipboard.writeText(randomCode)
+// }
+
+// getColor()
+
+// btn.addEventListener("click",getColor)
+
+
+
+// let abc = ()=>{
+// }
